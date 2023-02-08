b82779e6c70ee4808c6bc45df8d383c91ff01a86
dramonc <david.casanova@veinsur.es>
2023-02-08 15:34:00 +0100

    Primer Commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react"
+
+
+
+export const AddCategory = ( { setCategories ,onNewCategory}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = ({target}) =>{
+        setInputValue(target.value);
+    }
+
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        const newInputValue = inputValue.trim();
+        
+        if(newInputValue.length <= 1) return;
+
+        onNewCategory( newInputValue );
+        setInputValue('')
+    }
+
+  return (
+    <form onSubmit={ (event) => onSubmit(event) }>
+        <input 
+            type="text"
+            placeholder="Buscar Gifs"
+            value={ inputValue }
+            onChange={ onInputChange }
+
+        />
+        
+    </form>
+  )
+}
+
